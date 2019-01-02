79b72b7e2cd918f3f1b2655a2d87288ec8e643c8
Arvid Edenheim <arved490@student.liu.se>
2019-01-02 02:07:17 +0100

    Updates

---
+++ precache-manifest.3e7d47a218a1ca8dafef3f121370704b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9670264cbd6d1fba7faa",
+    "url": "/static/js/main.9670264c.chunk.js"
+  },
+  {
+    "revision": "c44f641cbfe8673f2c78",
+    "url": "/static/js/1.c44f641c.chunk.js"
+  },
+  {
+    "revision": "9670264cbd6d1fba7faa",
+    "url": "/static/css/main.a6931823.chunk.css"
+  },
+  {
+    "revision": "e8aba0c905e453fa71f0ef4e72f28757",
+    "url": "/index.html"
+  }
+];
