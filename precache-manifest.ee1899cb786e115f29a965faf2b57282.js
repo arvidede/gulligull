b5735e0a6f1e9ba557565a1859901f1fa552a6f3
Arvid Edenheim <arved490@student.liu.se>
2019-01-03 00:54:53 +0100

    Updates

---
+++ precache-manifest.ee1899cb786e115f29a965faf2b57282.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "62aaba37ef4c13f67ff0561ef29170a1",
+    "url": "/gulligull/static/media/sign-out-alt-solid.62aaba37.svg"
+  },
+  {
+    "revision": "de43668b4b4adb027fb9adc2ff6c8879",
+    "url": "/gulligull/static/media/heart-solid.de43668b.svg"
+  },
+  {
+    "revision": "0284cafb8caff9e84db4",
+    "url": "/gulligull/static/js/runtime~main.0284cafb.js"
+  },
+  {
+    "revision": "789361f2c0f818fc9734",
+    "url": "/gulligull/static/js/main.789361f2.chunk.js"
+  },
+  {
+    "revision": "75d1167dccf38e65ac9c",
+    "url": "/gulligull/static/js/1.75d1167d.chunk.js"
+  },
+  {
+    "revision": "789361f2c0f818fc9734",
+    "url": "/gulligull/static/css/main.13145481.chunk.css"
+  },
+  {
+    "revision": "ab7fe4df2d30903ac1585deb926984ca",
+    "url": "/gulligull/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gulligull/precache-manifest.8269ffac95d5d9c15a94767192c7d9a1.js"
+  "/gulligull/precache-manifest.ee1899cb786e115f29a965faf2b57282.js"
 );
 
 workbox.clientsClaim();
